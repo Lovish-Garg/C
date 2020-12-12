92572b2aec9e003149a9794edd1fd85d4c1803e5
Lovish Garg <70561590+Lovish-Garg@users.noreply.github.com>
2020-12-12 20:45:04 +0530

    create

--- Algorithms/Sorting/QuickSort.c
+++ Algorithms/Sorting/QuickSort.c
@@ -10,7 +10,7 @@ int main(void)
 	printf("Testcases: ");
 	scanf("%d", &t);
 
-	while (t--)
+	while(t--)
 	{
 		int n;
 
@@ -19,49 +19,59 @@ int main(void)
 
 		int arr[n];
 
-		printf("\nInput data in array->\n");
+		printf("\nInput data in elements->\n");
 		for (int i = 0; i < n; i++)
 			scanf("%d", &arr[i]);
 
-		printf("\nBefore Sorting->\n");
-		print_arr(arr, n);
-		quicksort(arr, 0, n - 1);
-		printf("After Sorting->\n");
-		print_arr(arr, n);
+		printf("\nArray before sorting->\n");
+		print_arr(arr, n);// displaying array before sorting
+
+		quicksort(arr, 0, n - 1);// calling qquicksort to sort the fucntion
+
+		printf("\n\nArray after sorting->\n");
+		print_arr(arr, n);// displaying array after sorting
+
+		printf("\n\n");
 	}
+
+	getc(stdin);
+	return 0;
 }
 
-void swap(int *num1, int *num2)
+// swaps the number 
+void swap(int *num, int *num2)
 {
-	int temp = *num1;
-	*num1 = *num2;
+	int temp = *num;
+	*num = *num2;
 	*num2 = temp;
 }
+
+// it swap and bring all numbers smaller than pivot to before of the pivot
 int partition(int arr[], int l, int r)
-{	
+{
+	int i = l;
 	int pivot = arr[r];
-	int i = l - 1;
 
 	for (int j = l; j < r; j++)
 	{
 		if (arr[j] < pivot)
 		{
-			i++;
-			swap(&arr[i], &arr[j]);
+			swap(&arr[j], &arr[i++]);
 		}
 	}
-	swap(&arr[i + 1], &arr[r]);
-	return i + 1;
+
+	swap(&arr[i], &arr[r]);
+	return i;
 }
 
 void quicksort(int arr[], int l, int r)
 {
 	if (l < r)
 	{
-		int pi = partition(arr, l, r);
+		int index  = partition(arr, l, r);
 
-		quicksort(arr, l, pi - 1);
-		quicksort(arr, pi + 1, r);
+		quicksort(arr, l, index - 1);
+		quicksort(arr, index, r);
 	}
 }
 
@@ -69,6 +79,4 @@ void print_arr(int arr[], int n)
 {
 	for (int i = 0; i < n; i++)
 		printf("%d ", arr[i]);
-
-	printf("\n\n");
 }
